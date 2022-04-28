d0d0ef94926d3031d5e0880eb43f21e0f4d85bb9
Dlray89 <dlrayjr89@gmail.com>
2022-04-28 12:56:08 -0400

    added mobile menu icon on responsivness

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import Logo from '../../images/logo.svg'
+import HamburgerIcon from '../../images/icon-hamburger.svg'
 
 
 
@@ -29,6 +30,12 @@ export const Navigation = () => {
                         // eslint-disable-next-line jsx-a11y/anchor-is-valid
                         <a className="nav__links-box--link" href="#">{link.name}</a>
                     ))}
+                </div>
+
+                <div className="nav__links-box-mobile">
+                    <button className="nav__links-box-mobile__btn">
+                      <img src={HamburgerIcon} alt='mobile menu icon' />
+                  </button>
                 </div>
 
             </div>
