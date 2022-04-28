93f801de62b85a8f4d306f5f9153245e976b0025
Dlray89 <dlrayjr89@gmail.com>
2022-04-28 14:05:51 -0400

    styling clean up and loosen up padding in main content for mobile

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -26,26 +26,26 @@ export const Navigation = () => {
         <>
 
             {openMobileNav ? (<>
-                <div className="nav__mobile-nav">
+                <div className={openMobileNav ? "nav__mobile-nav--deactive" : "nav__mobile-nav--active"}>
 
-                    <div className="nav__mobile-nav__mobile-box">
-                        <div className="nav__mobile-nav__mobile-box__logo-box-img">
-                            <img alt='logo' className="nav__mobile-nav__mobile-box__logo-box-img--img" src={Logo} />
+                    <div className={openMobileNav ? "nav__mobile-nav--deactive__mobile-box" : "nav__mobile-nav--active__mobile-box"}>
+                        <div className={openMobileNav ? "nav__mobile-nav--deactive__mobile-box__logo-box-img" : "nav__mobile-nav--active__mobile-box__logo-box-img"}>
+                            <img alt='logo' className={openMobileNav ? "nav__mobile-nav--deactive__mobile-box__logo-box-img--img" : "nav__mobile-nav--active__mobile-box__logo-box-img--img"} src={Logo} />
                         </div>
 
 
-                        <div className="nav__mobile-nav__mobile-box__cancel-btn">
-                            <button onClick={closeNav} className="nav__mobile-nav__mobile-box__cancel-btn--btn">
-                                <img src={CloseIcon} />
+                        <div className={openMobileNav ? "nav__mobile-nav--deactive__mobile-box__cancel-btn" : "nav__mobile-nav--active__mobile-box__cancel-btn"}>
+                            <button onClick={closeNav} className={openMobileNav ? "nav__mobile-nav--deactive__mobile-box__cancel-btn--btn" : "nav__mobile-nav--active__mobile-box__cancel-btn--btn"}>
+                                <img alt='close icon' src={CloseIcon} />
                             </button>
                         </div>
                     </div>
 
 
-                    <div className="nav__mobile-nav__links-box">
-                        <div className="nav__mobile-nav__links-box__links">
+                    <div className={openMobileNav ? "nav__mobile-nav--deactive__links-box" : "nav__mobile-nav--active__links-box"}>
+                        <div className={openMobileNav ? "nav__mobile-nav--deactive__links-box__links" : "nav__mobile-nav--active__links-box__links"}>
                             {navList.map(link => (
-                                <a className="nav__mobile-nav__links-box--link">{link.name}</a>
+                                <a href="https://loopstudios-mu.vercel.app/" className={openMobileNav ? "nav__mobile-nav--deactive__links-box--link": "nav__mobile-nav--active__links-box--link"}>{link.name}</a>
                             ))}
                         </div>
                     </div>
@@ -62,7 +62,7 @@ export const Navigation = () => {
                     <div className="nav__links-box">
                         {navList.map(link => (
                             // eslint-disable-next-line jsx-a11y/anchor-is-valid
-                            <a className="nav__links-box--link" href="#">{link.name}</a>
+                            <a className="nav__links-box--link" href="https://loopstudios-mu.vercel.app/">{link.name}</a>
                         ))}
                     </div>
 
